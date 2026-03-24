292d196aa4fce377e1a4749ced138d25fdf38343
VictorJS23 <vb329708@gmail.com>
2026-03-24 14:59:33 -0600

    Convert divition solve

--- NotacionDiv/main4.js
+++ NotacionDiv/main4.js
@@ -13,3 +13,45 @@ function decimalABinario(n) {
 }
 
 console.log(decimalABinario(num)); // "100011"
+
+// Parte Hexadecimal
+
+let numH = 62
+let baseH = 16
+
+let residuos = [];
+
+function decimalHexadecimal(n) {
+
+    while (n > 0) {
+        residuos.push(n % baseH); // guardas el residuo
+        n = Math.floor(n / baseH); // divides entre 2
+    }
+
+    return residuos.reverse()
+}
+
+console.log(decimalHexadecimal(numH))
+
+const Equals = {
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+};
+
+let nuevoArr = []
+
+for (let i = 0; i < residuos.length; i++) {
+    if (residuos[i] >= 10) {
+        nuevoArr.push(Equals[residuos[i]])
+    } else {
+        nuevoArr.push(residuos[i])
+    }
+}
+
+let final = nuevoArr.join("")
+
+console.log(final)
