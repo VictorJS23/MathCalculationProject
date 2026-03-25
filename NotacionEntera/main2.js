fc7d2f7195a287e2b95704471bf839ac5cacbbf5
VictorJS23 <vb329708@gmail.com>
2026-03-24 22:07:01 -0600

    Int Notation solve

--- NotacionEntera/main2.js
+++ NotacionEntera/main2.js
@@ -1,64 +1,64 @@
+// || YA FUNCIONA NO TOCAR ||
+
+//----------Import de funciones----------------->>
+import { IsThereANumber } from "../validation"
+//----------Import de funciones----------------->>
+
 // Solo para Notaciones enteras (sin punto decimal)
+export const anyIntNotationToDecimal = (num, base) => {
 
-let num = 3304
-let base = 5 // Cualquier notacion a decimal
+    if (!IsThereANumber(num)) {
+        let arr = String(num).split('').map(Number);
 
-let arr = String(num).split('').map(Number);
+        let result = 0
 
-console.log(arr);
+        for (let i = 0; i < arr.length; i++) {
+            result = result * base + arr[i];
+        }
+        console.log(result);
 
-let result = 0
-
-for (let i = 0; i < arr.length; i++) {
-    result = result * base + arr[i];
-}
-
-console.log(`El numero "${num}" de base "${base}" en decimal es : ${result}`)
-
-//---------------Notacion Hexadecimal-------------->
-
-let hex = "2ad"
-let baseHex = 16
-
-let arrHex = String(hex).split('');
-
-let Equals = {
-    a: 10,
-    b: 11,
-    c: 12,
-    d: 13,
-    e: 14,
-    f: 15,
-};
-
-let nuevoArr = [];
-
-for (let i = 0; i < arrHex.length; i++) {
-    let valor = arrHex[i].toLowerCase();
-
-    if (Equals.hasOwnProperty(valor)) {
-        // Si es letra hexadecimal, la convertimos
-        nuevoArr.push(Equals[valor]);
     } else {
-        nuevoArr.push(arrHex[i]);
+        let arrHex = String(num).split('');
+
+        let Equals = {
+            a: 10,
+            b: 11,
+            c: 12,
+            d: 13,
+            e: 14,
+            f: 15,
+        };
+
+        let nuevoArr = [];
+
+        for (let i = 0; i < arrHex.length; i++) {
+            let valor = arrHex[i].toLowerCase();
+
+            if (Equals.hasOwnProperty(valor)) {
+                // Si es letra hexadecimal, la convertimos
+                nuevoArr.push(Equals[valor]);
+            } else {
+                nuevoArr.push(arrHex[i]);
+            }
+        }
+        console.log(nuevoArr)
+
+        let arrResultado = [];
+
+        for (let i = 0; i < nuevoArr.length; i++) {
+            arrResultado.push(Number(nuevoArr[i])); // convertir a entero
+
+        }
+        console.log(arrResultado);
+
+        let resultHex = 0
+
+        for (let i = 0; i < arrResultado.length; i++) {
+            resultHex = resultHex * base + arrResultado[i];
+        }
+        console.log(resultHex);
     }
 }
 
-console.log(nuevoArr)
+anyIntNotationToDecimal(1011011, 2)
 
-let arrResultado = [];
-
-for (let i = 0; i < nuevoArr.length; i++) {
-    arrResultado.push(Number(nuevoArr[i])); // convertir a entero
-
-}
-
-console.log(arrResultado);
-
-let resultHex = 0
-
-for (let i = 0; i < arrResultado.length; i++) {
-    resultHex = resultHex * baseHex + arrResultado[i];
-}
-
-console.log(`El numero "${hex}" de base "${baseHex}" en decimal es : ${resultHex}`)
--- validation.js
+++ validation.js
@@ -1,3 +1,23 @@
-document.getElementById("convert").addEventListener("click", function () {
-    alert("Funciona el botón");
+//----------Import de funciones----------------->>
+import { anyIntNotationToDecimal } from "./NotacionEntera/main2";
+//----------Import de funciones----------------->>
+
+// Valida si hay una letra en el Input
+export const IsThereANumber = (number) => {
+    return Array.from(number).some(c => /[a-zA-Z]/.test(c));
+}
+
+//--------------test------------------->>
+const btn = document.getElementById("convert");
+
+btn.addEventListener("click", function () {
+    const numero = document.getElementById("input").value;
+
+    if (numero === "") {
+        swal("No has ingresado ningun numero!!");
+        return;
+    }
+
+    swal("Número ingresado: " + numero);
 });
+
